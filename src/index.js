91ee429fc717d9233b104ef9e31714a9be886d61
TheoMNU <meneu.theo@gmail.com>
2023-12-25 17:45:16 +0100

    v0.2

--- src/index.js
+++ src/index.js
@@ -16,3 +16,26 @@ introHidden()
 mainVisible()
 
 // ------------------------------------------------------
+let projects = [
+    `<a href="../../"
+><i class="fa-brands fa-spotify"></i>Spoticlone</a
+>`,
+    `<a href="#"
+><i class="fa-solid fa-worm"></i>Men in blate</a
+>`,
+]
+
+const realList = document.querySelector('#realList')
+
+function projectList() {
+    const realLink = document.createElement('li')
+    realList.append(realLink)
+
+    for (let i = 0; i <= projects.length - 1; i++) {
+        realLink.innerHTML += `
+        ${projects[i]}
+		`
+    }
+}
+
+projectList()
